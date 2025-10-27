79143e1769c55b0e00206ed8ba0f4427f004acd7
Tiago Dias <tiago@tdias.pt>
2025-10-27 01:08:23 +0000

    add more colors

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -21,6 +21,18 @@ const COLOR_SCALE = [
   '#7f7f7f',
   '#bcbd22',
   '#17becf',
+  '#8dd3c7',
+  '#ffffb3',
+  '#bebada',
+  '#fb8072',
+  '#80b1d3',
+  '#fdb462',
+  '#b3de69',
+  '#fccde5',
+  '#d9d9d9',
+  '#bc80bd',
+  '#ccebc5',
+  '#ffed6f',
 ];
 
 export function meta({}: Route.MetaArgs) {
